b1d0fdfdce01d9f2df35fa94a6eb5d39a88bfe9f
Jorge Castelló <jcaste999@gmail.com>
2022-09-30 13:20:09 +0200

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import path from "path";
-const isGitHubPages = true;
-const folderName = path.basename(process.cwd()) + "/";
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+import path from 'path'
+const isGitHubPages = true
+const folderName = path.basename(process.cwd()) + '/'
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
+const base = mode === 'production' && isGitHubPages ? '/' + folderName : '/'
 
 export default {
-  root: "src",
+  root: 'src',
   base,
   mode,
-  publicDir: "../public",
+  publicDir: '../public',
   build: {
-    outDir: "../dist",
-    assetsDir: "./"
+    outDir: '../dist',
+    assetsDir: './'
   }
-};
+}

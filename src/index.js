77d08d8214655e3f2262f32336aec039f347cf71
Jorge Castelló <jcaste999@gmail.com>
2022-09-30 11:46:50 +0200

    Añadir boton al lado de la card, poner icono en boton con cambio de color segun color de fondo de card y quitar borde card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const BUTTON_THEMES = document.getElementsByClassName("button-themes");
+const BUTTON_THEMES = document.getElementsByClassName('button-themes')
 
-let debounc = false;
-function changeThemes(color) {
+let debounc = false
+function changeThemes (color) {
   if (debounc) {
-	document.documentElement.style.setProperty('--card-color', '#ffffff');
-	document.documentElement.style.setProperty('--font-color', '#000000');   
-    debounc = false;
+    document.documentElement.style.setProperty('--card-color', '#ffffff')
+    document.documentElement.style.setProperty('--font-color', '#000000')
+    document.documentElement.style.setProperty('--image-change', 'url("img/modo-oscuro-white.png")')
+    document.documentElement.style.setProperty('--color-change', '#181820')
+    debounc = false
   } else {
-    document.documentElement.style.setProperty('--card-color', '#181820');
-	document.documentElement.style.setProperty('--font-color', '#ffffff');
-    debounc = true;
+    document.documentElement.style.setProperty('--card-color', '#181820')
+    document.documentElement.style.setProperty('--font-color', '#ffffff')
+    document.documentElement.style.setProperty('--image-change', 'url("img/modo-oscuro.png")')
+    document.documentElement.style.setProperty('--color-change', '#ffffff')
+    debounc = true
   }
   // console.log()
 }
 
-BUTTON_THEMES[0].addEventListener("click", function() { changeThemes(); });
+BUTTON_THEMES[0].addEventListener('click', function () { changeThemes() })
